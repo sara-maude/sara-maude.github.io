67ec3cdb29c61752b1d6fe66ebe9609b39d8ea5f
Sara-Maude <sara-maude.laliberte@polymtl.ca>
2025-11-18 01:47:22 -0500

    ajout toolbar + amélioration display jeu

--- JS/jeu.js
+++ JS/jeu.js
@@ -2,33 +2,57 @@ document.getElementById("back").addEventListener("click", function() {
     window.location.href = "accueil.html";
 });
 
-const element = "<div class='piece'></div>";
-let boardHTML = "";
-
-for (let i = 0; i < 8; i++) {
-    let line = "<div class='line'>";
-    for (let j = 0; j < 4; j++) {
-        line += element;
-    }
-    line += "</div>";
-    boardHTML += line;
-}
-
-document.getElementById("board").innerHTML = boardHTML;
-
 const couleur = "<div class='piece choix' ";
-let paletteHTML = "";
 const couleurs = ["#FF0000","#FF7300", "#FFF200", "#35D300", "#00B7FF", "#9900FF", "#FE00E5", "#0400FF"];
 
-for (let i = 0; i < 2; i++) {
-    let line = "<div class='line'>";
-    for (let j = 0; j < 3; j++) {
-        const index = i === 0 ? j : j + 3;
-        const style = `background-color:${couleurs[index]};`;
-        line += `${couleur} style="${style}"></div>`;
-    }
-    line += "</div>";
-    paletteHTML += line;
-}
+const element = "<div class='piece'></div>";
 
-document.getElementById("palette").innerHTML = paletteHTML;
+displayGame(8,4,2,3);
+
+const debutant = document.getElementById("debutant");
+const avance = document.getElementById("avance");
+
+debutant.addEventListener("click", () => {
+    if (debutant.classList.contains("active")){
+        debutant.classList.replace("active", "selected");
+        avance.classList.replace("selected", "active");
+        displayGame(8,4,2,3);
+    }
+})
+
+avance.addEventListener("click", () => {
+    if (avance.classList.contains("active")){
+        avance.classList.replace("active", "selected");
+        debutant.classList.replace("selected", "active");
+        displayGame(10,5,2,4);
+    }
+})
+
+function displayGame(rowboard, columnboard, rowcolor, columncolor){
+    let boardHTML = "";
+    let paletteHTML = "";
+
+    for (let i = 0; i < rowboard; i++) {
+        let line = "<div class='line'>";
+        for (let j = 0; j < columnboard; j++) {
+            line += element;
+        }
+        line += "</div>";
+        boardHTML += line;
+    }
+
+    document.getElementById("board").innerHTML = boardHTML;
+
+    for (let i = 0; i < rowcolor; i++) {
+        let line = "<div class='line'>";
+        for (let j = 0; j < columncolor; j++) {
+            const index = i === 0 ? j : j + columncolor;
+            const style = `background-color:${couleurs[index]};`;
+            line += `${couleur} style="${style}"></div>`;
+        }
+        line += "</div>";
+        paletteHTML += line;
+    }
+
+    document.getElementById("palette").innerHTML = paletteHTML;
+}
