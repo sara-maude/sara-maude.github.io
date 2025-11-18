ef7124ce84b0be55c87cba55418aa7aca2d973ea
Sara-Maude <sara-maude.laliberte@polymtl.ca>
2025-11-17 23:52:30 -0500

    petites modifs accueil + début jeu display

--- JS/accueil.js
+++ JS/accueil.js
@@ -14,42 +14,30 @@ document.getElementById("trophy").addEventListener("click", function() {
     window.location.href = "historique.html";
 });
 
-document.getElementById("light").addEventListener("click", () => {
-    const root = document.documentElement;
-    const fonce = getComputedStyle(root).getPropertyValue("--fonce");
-    const pale  = getComputedStyle(root).getPropertyValue("--pale");
-
-    root.style.setProperty("--fonce", pale);
-    root.style.setProperty("--pale", fonce);
-
-    const icon = document.querySelector("#light span");
-
-    if (icon.textContent.trim() === "light_off") {
-        icon.textContent = "lightbulb";
-    } else {
-        icon.textContent = "light_off";
-    }
-});
-
 const robot  = document.querySelector("#robot");
 const friend = document.querySelector("#group");
 const checkbox = document.querySelector("#toggle");
 
 checkbox.addEventListener("change", () => {
     const robotFilled = robot.classList.contains("filled");
+    let mode;
 
     if (robotFilled) {
+        mode = "ami";
         robot.classList.remove("filled");
         robot.classList.add("unfilled");
 
         friend.classList.remove("unfilled");
         friend.classList.add("filled");
     } else {
+        mode = "bot";
         robot.classList.remove("unfilled");
         robot.classList.add("filled");
 
         friend.classList.remove("filled");
         friend.classList.add("unfilled");
     }
+
+    localStorage.setItem("gameMode", mode);
 });
 
